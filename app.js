07de3fa96406d070abac93602dc3a29c8d7346f7
prohamburger <bacuong1107@gmail.com>
2020-01-05 20:53:48 +0700

    fix

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/register', async (req, res) => {
 });
 
 //connect DB
-mongoose.connect (`${process.env.MONGO_URI}`, { useNewUrlParser: true }, () => console.log('connected to DB'))
+mongoose.connect (`${process.env.MONGODB_URI}`, { useNewUrlParser: true }, () => console.log('connected to DB'))
 app.listen(port)
 
 
